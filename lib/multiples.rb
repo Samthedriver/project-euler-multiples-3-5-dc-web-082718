e8c5d99cd0ca1a6736246bfae106d04bf1a80095
github username <github email address>
2019-03-24 13:41:56 -0400

    Sum of multiples of 3 or 5 under 1000.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,18 @@
 # Enter your procedural solution here!
+def multiples
+
+  x = 999;
+  total = 0;
+
+  for i in 1..x do
+    if (i % 3 == 0) || (i % 5 == 0)
+      puts i;
+      total += i;
+    end
+  end
+
+  puts total
+
+end
+
+multiples
